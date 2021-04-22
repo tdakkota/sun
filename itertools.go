e82680cf5c5d10e821384a21ce97ce557989b91e
Milad Nasrollahi <milad.m.nasr@gmail.com>
2021-04-21 23:21:27 -0700

    Allow count to accept floats, nan, inf and remove unused struct value
    The previous iteration of count only handled integer values. An attempt
    is made here to allow float inputs alongside integer ones. That means
    count(0.5, 3), count(float('nan'), 2), etc are now valid inputs and
    should yield outputs similar to Python's itertools.count.
    
    A value of the struct countObject is also removed since it was doing
    nothing.

--- itertools.go
+++ itertools.go
@@ -6,24 +6,115 @@ import (
 	"go.starlark.net/starlark"
 )
 
-type countObject struct {
-	cnt    int
-	step   int
-	frozen bool
-	value  starlark.Value
+// Type that attempts to allow operations between numerics,
+// i.e. float and int.
+type floatOrInt struct {
+	f_ *starlark.Float
+	i_ *starlark.Int
 }
 
-func newCountObject(cnt int, stepValue int) *countObject {
-	return &countObject{cnt: cnt, step: stepValue, value: starlark.MakeInt(cnt)}
+// Unpacker for float or int type. This allows int types and float
+// types to interact with one another, e.g. count(0, 0.1).
+// type floatOrInt float6
+func (p *floatOrInt) Unpack(v starlark.Value) error {
+	errorMsg := "floatOrInt must have default initialization"
+
+	switch v := v.(type) {
+	case starlark.Int:
+		if p.f_ != nil {
+			return fmt.Errorf(errorMsg)
+		}
+		p.i_ = &v
+		return nil
+	case starlark.Float:
+		if p.i_ != nil {
+			return fmt.Errorf(errorMsg)
+		}
+		p.f_ = &v
+		return nil
+	}
+	return fmt.Errorf("got %s, want float or int", v.Type())
+}
+
+func (fi *floatOrInt) add(n floatOrInt) error {
+	switch {
+	case fi.i_ != nil && n.i_ != nil:
+		x := fi.i_.Add(*n.i_)
+		fi.i_ = &x
+		return nil
+	case fi.i_ != nil && n.f_ != nil:
+		x := starlark.Float(float64(fi.i_.Float()) + float64(*n.f_))
+		fi.f_ = &x
+		fi.i_ = nil
+		return nil
+	case fi.f_ != nil && n.i_ != nil:
+		x := starlark.Float(float64(*fi.f_) + float64(n.i_.Float()))
+		fi.f_ = &x
+		fi.i_ = nil
+		return nil
+	case fi.f_ != nil && n.f_ != nil:
+		x := starlark.Float(float64(*fi.f_) + float64(*n.f_))
+		fi.f_ = &x
+		return nil
+	}
+	return fmt.Errorf("float to int addition not possible")
+}
+
+func (fi floatOrInt) string() string {
+	switch {
+	case fi.i_ != nil:
+		return fi.i_.String()
+	case fi.f_ != nil:
+		return fi.f_.String()
+	default:
+		// This block should not be reached.
+		// starlark's String() method is being replicated
+		// so an error is not raised.
+		return ""
+	}
+}
+
+// Equality operator between floatOrInt and starlark's Int, Float
+// and Golang's int.
+func (fi *floatOrInt) eq(v interface{}) bool {
+	switch v := v.(type) {
+	case starlark.Int:
+		if fi.i_ != nil && *fi.i_ == v {
+			return true
+		} else {
+			return false
+		}
+	case starlark.Float:
+		if fi.f_ != nil && *fi.f_ == v {
+			return true
+		} else {
+			return false
+		}
+	case int:
+		if fi.i_ == nil {
+			return false
+		}
+		var x int
+		starlark.AsInt(*fi.i_, &x)
+		return x == v
+	}
+
+	return false
+}
+
+type countObject struct {
+	cnt    floatOrInt
+	step   floatOrInt
+	frozen bool
 }
 
 func (co *countObject) String() string {
 	// As with the cpython implementation, we don't display
-	// step when it is an integer equal to 1.
-	if co.step == 1 {
-		return fmt.Sprintf("count(%v)", co.cnt)
+	// step when it is an integer equal to 1 (default step value).
+	if co.step.eq(1) {
+		return fmt.Sprintf("count(%v)", co.cnt.string())
 	}
-	return fmt.Sprintf("count(%v, %v)", co.cnt, co.step)
+	return fmt.Sprintf("count(%v, %v)", co.cnt.string(), co.step.string())
 }
 
 func (co *countObject) Type() string {
@@ -33,7 +124,6 @@ func (co *countObject) Type() string {
 func (co *countObject) Freeze() {
 	if !co.frozen {
 		co.frozen = true
-		co.value.Freeze()
 	}
 }
 
@@ -58,8 +148,16 @@ func (c *countIter) Next(p *starlark.Value) bool {
 	if c.co.frozen {
 		return false
 	}
-	*p = starlark.MakeInt(c.co.cnt)
-	c.co.cnt += c.co.step
+
+	switch {
+	case c.co.cnt.i_ != nil:
+		*p = c.co.cnt.i_
+	case c.co.cnt.f_ != nil:
+		*p = c.co.cnt.f_
+	}
+
+	c.co.cnt.add(c.co.step)
+
 	return true
 }
 
@@ -72,37 +170,28 @@ func count_(
 	kwargs []starlark.Tuple,
 ) (starlark.Value, error) {
 	var (
-		start int
-		step  int
+		defaultStart            = starlark.MakeInt(0)
+		defaultStep             = starlark.MakeInt(1)
+		start        floatOrInt = floatOrInt{}
+		step         floatOrInt = floatOrInt{}
 	)
 
 	if err := starlark.UnpackPositionalArgs(
 		"count", args, kwargs, 0, &start, &step,
 	); err != nil {
 		return nil, fmt.Errorf(
-			"Got %v but expected NoneType or valid integer values for "+
-				"start and step, such as (0, 1).", args.String(),
+			"Got %v but expected no args, or one or two valid numbers",
+			args.String(),
 		)
 	}
 
-	const (
-		defaultStart = 0
-		defaultStep  = 1
-	)
-	// The rules for populating the count object based on the number
-	// of args passed is as follows:
-	// 	0 args -> default values for start and step
-	// 	1 args -> arg defines start, default for step
-	// 	2 args -> both start and step are defined by args
-	var co_ *countObject
-	switch nargs := len(args); {
-	case nargs == 0:
-		co_ = newCountObject(defaultStart, defaultStep)
-	case nargs == 1:
-		co_ = newCountObject(start, defaultStep)
-	default: // nargs == 2
-		co_ = newCountObject(start, step)
+	// Check if start or step require default values.
+	if start.f_ == nil && start.i_ == nil {
+		start.i_ = &defaultStart
+	}
+	if step.f_ == nil && step.i_ == nil {
+		step.i_ = &defaultStep
 	}
 
-	return co_, nil
+	return &countObject{cnt: start, step: step}, nil
 }
